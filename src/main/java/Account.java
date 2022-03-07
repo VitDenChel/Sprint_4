e409b2efdb0babfb5320a841f155f1387fd12be9
Vitalii Denisiuk <vdenisyuk-78@yandex.ru>
2022-03-07 23:34:49 +0500

    Commit Denisiuk Vitalii

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,20 +1,13 @@
 public class Account {
     private final String name;
-    int counter = 0;
 
     public Account(String name) {
         this.name = name;
     }
 
     public boolean checkNameToEmboss() {
-        if (name.length() >= 3 && name.length() <= 19) {
-            for (int i = 0; i < name.length(); i++) {
-                if (name.charAt(i) == ' ') {
-                    counter++;
-                }
-            }
-        }
-        if (counter == 1 && (!name.startsWith(" ") && !name.endsWith(" "))) {
+        int spaces = name.length() - name.replace(" ", "").length();
+        if (name.length() >= 3 && name.length() <= 19 && (!name.startsWith(" ")) && (!name.endsWith(" ")) && (name.indexOf(' ') != -1) && (spaces == 1 )) {
             return true;
         }
         return false;
@@ -26,16 +19,6 @@ public class Account {
 
 
 
-
-
-
-
-
-        /*
-
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-         */
 
 
 
